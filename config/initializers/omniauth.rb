8369f9e6e18bf88cb7931a1083cabbbdd609c864
agatov <agatovs@gmail.com>
2012-10-18 17:03:42 +0400

    vkontakte settings changed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :vkontakte, '2812000', 'Xbyc8oTXh5KzQK52E2em', {
+  provider :vkontakte, '3036832', 'd3kg1Zf7gQ7wd7Lbbs6R', {
       scope: "notify,friends,photos"
   }
 
