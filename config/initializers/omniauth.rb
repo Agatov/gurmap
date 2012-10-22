bb2d8c2a1e4f405fd801e1db0ba98def17b7aae7
agatov <agatovs@gmail.com>
2012-10-22 22:24:45 +0400

    offline access to vk api

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :vkontakte, '3036832', 'd3kg1Zf7gQ7wd7Lbbs6R', {
-      scope: "notify,friends,photos"
+      scope: "notify,friends,photos,offline"
   }
 
 
