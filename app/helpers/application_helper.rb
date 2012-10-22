83c73e7d078ff88bf9de5b2d45a638619dd9078c
agatov <agatovs@gmail.com>
2012-10-22 22:00:41 +0400

    social params for vk repost

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,17 @@ module ApplicationHelper
   def logout
     session[:user_id] = nil
   end
+
+  def order_social_params(place_name, order_id)
+    return {
+      url: "http://#{request.host}/#{order_id}",
+      title: CGI.escape("I get 30% sale to #{place_name} with Gurmap"),
+      description: CGI.escape(I18n.translate("OLOLOLO"),
+      image: "http://#{request.host}/images/logo.png"
+    }
+  end
+
+  def vk_social_url(params)
+    "http://vk.com/share.php?url=#{params[:url]}&title=#{params[:title]}&image=#{params[:image]}&description=#{params[:description]}&noparse=true"
+  end
 end
